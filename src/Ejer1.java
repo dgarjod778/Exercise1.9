d0a8b72d74da993fdb17852d15110b0d670b99da
David García Jodar <dgarjod778@gmail.com>
2021-11-29 18:22:03 +0100

    Ejercicios terminados

---
+++ src/Ejer1.java
@@ -0,0 +1,40 @@
+
+public class Ejer1 {
+
+	public static void main(String[] args) {
+		/*
+		 * Investiga en la API de Java para qué sirven los siguientes métodos de la
+		 * clase Math. Hacen el Eclipse un ejemplo de utilización para cada uno
+		 */
+
+		// 1.-
+		System.out.println(Math.abs(-5)); // Me da un numero absoluto.
+
+		// 2.-
+		System.out.println(Math.addExact(5, 10)); // suma los dos numeros
+
+		// 3.-
+		System.out.println(Math.ceil(2.2)); // redondea los numeros.
+		System.out.println(Math.ceil(-0.2));
+
+		// 4.-
+		System.out.println(Math.decrementExact(-2)); // decrece en uno el numero.
+
+		// 5.-
+		System.out.println(Math.floor(10));
+
+		// 6.-
+		System.out.println(Math.incrementExact(5));
+
+		// 7.-
+		System.out.println(Math.max(6, 5));
+
+		// 8.-
+		System.out.println(Math.multiplyExact(2, 4));
+
+		// 9.-
+		System.out.println(Math.negateExact(-10));
+
+	}
+
+}
---
+++ src/Ejer2.java
@@ -0,0 +1,22 @@
+
+public class Ejer2 {
+
+	public static void main(String[] args) {
+		String s1 = "hola";
+		// 1.-
+		char a = s1.charAt(0);
+		System.out.println(a);
+
+		// 2.-
+		int tamaño = s1.length();
+		System.out.println(tamaño);
+
+		// 3.-
+		int posicion = s1.codePointAt(a);
+		System.out.println(posicion);
+		
+		//4.-
+		int 
+	}
+
+}
